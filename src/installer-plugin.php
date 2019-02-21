f67306dba80282278e0e7ac6688abd0426649fc9
MattYoke <matt.s@yokedesign.com.au>
2019-02-21 14:37:23 +1100

    init

---
+++ src/installer-plugin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Yoke\Composer;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class InstallerPlugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $installer = new TemplateInstaller($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+    }
+}
